9039f8df1b00154ad0c53ecfb7745a69d19b003c
= <=>
2024-02-02 16:46:56 +0530

    commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import *
-
-
+from pafy import new
+from pywhatkit import playonyt
 
 
 web = Flask(__name__)
-
+song = None
 
 
 @web.route("/link")
@@ -14,6 +14,7 @@ def home():
 
 @web.route("/you")
 def you():
+    global song
     args = request.args
     frm = args["from"]
     to = args["to"]
@@ -23,8 +24,12 @@ def you():
 
 @web.route("/iloveu")
 def love():
-    return render_template("love.html")
+    url = None
+    if song!=None:
+        url = new(playonyt(song.replace("%"," "),open_video=False).split("\\")[0]).getbestaudio().url
+
+    return render_template("love.html",url=url)
 
 
 
-web.run(debug=True)
+web.run(host="",debug=True)
