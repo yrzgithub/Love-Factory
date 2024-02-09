f86bfbecafc87bc2557db56e07497af19b888b0b
yrzgithub <85425005+yrzgithub@users.noreply.github.com>
2024-02-09 06:50:27 +0000

    commit

--- main.py
+++ main.py
@@ -54,8 +54,3 @@ def love():
         return "<error>Error Code : 420<br>Change the query and try again.</error>"
 
     return render_template("love.html",url=url)
-
-
-
-if __name__ == "__main__":
-    web.run(debug=True)
